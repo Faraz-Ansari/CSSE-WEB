89bc89f43da8248873e76e83b8dead08510b4a09
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-09-25 23:40:48 +0530

    create Event page template

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import './Css/locomotive-scroll.css';
-import Hero from './Hero/Hero';
-import CssePage from './Pages/Societypages/CssePage';
-import SocietyShowCard from './Society/SocietyShow';
-import InfoForm from './InfoForm/InfoForm';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./Css/locomotive-scroll.css";
+import Hero from "./Hero/Hero";
+import CssePage from "./Pages/Societypages/CssePage";
+import SocietyShowCard from "./Society/SocietyShow";
+import InfoForm from "./InfoForm/InfoForm";
+
+import Event from "./Pages/AllSociety/Event";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Hero />} />
-        
-        <Route path='/csse' element={<CssePage />} />
-        <Route path='/showSo' element={<SocietyShowCard/>}/>
-        <Route path='/infoform' element={<InfoForm/>}/>
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Hero />} />
+                <Route path="/event" element={<Event />} />
+
+                <Route path="/csse" element={<CssePage />} />
+                <Route path="/showSo" element={<SocietyShowCard />} />
+                <Route path="/infoform" element={<InfoForm />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Pages/AllSociety/Event.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Event() {
+    return (
+        <div
+            className="bg-slate-950 h-screen text-blue-700 font-bold text-5xl flex 
+        items-center justify-center text-center"
+        >
+            Events
+        </div>
+    );
+}
